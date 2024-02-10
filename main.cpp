0c5a48fe5f13385380ee8105d880dd979dfe1dc5
tahacmv <tmmotassimahmedtaha@gmail.com>
2024-02-10 16:09:10 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+int main() {
+
+    //Initialize socket
+
+    // Create a socket
+
+    //Bind the socket to an ip address and port
+
+    // Tell Winsock the socket is for listening
+
+    // Wait for a connection
+
+    // Close listening socket
+
+    // While loop: accept and echo message back to client
+
+    //Close the sock
+
+    //Shutdown winsock
+
+    return 0;
+}
